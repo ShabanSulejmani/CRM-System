a50739a1a892a215e4dfb4c067c2c19c74bb43cb
Hultberg80 <hultberg80@gmail.com>
2025-02-06 10:17:36 +0100

    changes

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -7,17 +7,22 @@ function Form(){
             <form>
                 <label htmlFor="firstname">First Name</label>
                 <input type="text" placeholder="Enter First Name" name="firstname" />
+                <br />
 
                 <label htmlFor="lastname">Last Name</label>
                 <input type="text" placeholder="Enter Last Name" name="lastname" />
+                <br />
 
                 <label htmlFor="email">Email</label>
                 <input type="text" placeholder="Enter Email" name="email" />
+                <br />
+
 
                 <label htmlFor="gender">Gender</label>
                 <input type="radio" name="gender" /> Male
-                <input type="radio" name="gender" /> Female
-                <input type="radio" name="gender" /> Other
+                <input type="radio" name="gender" /> Female 
+                <br />
+                <br />
 
                 <label htmlFor="subject">Subject</label>
                 <select name="subject" id="subject">
@@ -28,13 +33,15 @@ function Form(){
 
                 <label htmlFor="resume">Resume</label>
                 <input type="file" placeholder="Select Resume" name="resume"/>
+                <br />
 
                 <label htmlFor="url">URL</label>
                 <input type="text" placeholder="Enter Image URL" name="url"/>
+                <br />
 
                 <label htmlFor="about">About</label>
+                <br />
                 <textarea name="about" id="about" cols="30" rows="10" placeholder="Enter description"></textarea>
-
                 <button type="button">Reset</button>
                 <button type="submit">Submit</button>
             </form>
