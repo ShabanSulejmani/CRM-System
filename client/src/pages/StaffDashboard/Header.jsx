2a3d0b1b797366e7bd59549bdfe37b1ac27e551a
ecthelionofthefountain-510 <lundstedt.kevin@gmail.com>
2025-02-11 15:23:05 +0100

    CS fakking S

--- client/src/pages/StaffDashboard/Header.jsx
+++ client/src/pages/StaffDashboard/Header.jsx
@@ -6,6 +6,7 @@ function Header(){
   return<>
   <header className="header-login">
       <nav className="login-nav">
+        <h1 className="project-name">WPT</h1>
         <a href="">
         <img src="/img/login.png" alt="Logga in" className="login-img"/>
           </a>
--- client/src/pages/StaffDashboard/Main.jsx
+++ client/src/pages/StaffDashboard/Main.jsx
@@ -3,7 +3,8 @@ function Main() {
 
   return <>
     <div className="main-container">
-      <aside className="staff-aside">Företagnamn
+      <aside className="staff-aside">
+        <h2 className="company-name">Företagnamn</h2>
         <div>Martin</div>
         <div>Ville</div>
         <div>Kevin</div>
@@ -12,21 +13,27 @@ function Main() {
         <div>Sebbe</div>
       Inloggad support
       </aside>
-    <div className="tasks">Tasks
+      <div className="tasks">
+        <h2 className="tasks-header">Tasks</h2>
       <div>Hjälp med batteri</div>
       <div>2</div>
       <div>3</div>
       <div>4</div>
-      <div>5</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+
     </div>
-    <div className="my-tasks">My Tasks
+      <div className="my-tasks">
+        <h2 className="my-tasks-header">My Tasks</h2>
       <div>1</div>
       <div>2</div>
       <div>Kassa bromsar</div>
       <div>4</div>
       <div>5</div>
     </div>
-    <div className="done">Done
+      <div className="done">
+        <h2 className="done-header">Done</h2>
       <div>1</div>
       <div>2</div>
       <div>3</div>
